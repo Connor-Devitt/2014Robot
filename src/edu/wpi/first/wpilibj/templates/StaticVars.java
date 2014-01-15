16a9e73b3240c81c80471443768c81305da59466
davidcox95 <davidcox95@192.168.6.183>
2014-01-15 12:58:31 -0500

    edited StaticVars class for joystick.

--- src/edu/wpi/first/wpilibj/templates/StaticVars.java
+++ src/edu/wpi/first/wpilibj/templates/StaticVars.java
@@ -6,6 +6,9 @@ public class StaticVars {
     //motor number channels
     public static int FRONT_LEFT_MOTOR = 1;
     public static int FRONT_RIGHT_MOTOR = 2;
-    public static int BACK_LEFT_MOTOR = 3;
-    public static int BACK_RIGHT_MOTOR = 4;
+    public static int REAR_LEFT_MOTOR = 3;
+    public static int REAR_RIGHT_MOTOR = 4;
+    
+    //joystick  port number
+    public static int JOYSTICK_PORT = 1;
 }
