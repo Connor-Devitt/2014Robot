160c1134bbc0ee82b61e6f2f7693b605a79ec507
davidcox95 <davidcox95@10.0.0.7>
2014-01-15 14:36:25 -0500

    testing changes

--- src/edu/wpi/first/wpilibj/templates/Team5181Robot.java
+++ src/edu/wpi/first/wpilibj/templates/Team5181Robot.java
@@ -10,6 +10,7 @@ package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+//Testing changes
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
