5a716b6118b7843d2e166ec207612c8e764b8492
davidcox95 <davidcox95@192.168.6.183>
2014-01-16 15:33:03 -0500

    created autonomous object

---
+++ src/edu/wpi/first/wpilibj/templates/Autonomous.java
@@ -0,0 +1,28 @@
+
+package edu.wpi.first.wpilibj.templates;
+
+import java.lang.String;
+import edu.wpi.first.wpilibj.Timer;
+
+public class Autonomous {
+    
+    private String status;
+    private boolean timerStarted;
+    private static Timer timer;
+    
+    
+    
+    public Autonomous(RobotHardware hardware) {
+        
+    }
+    
+    public boolean isTimerStarted() {
+        return timerStarted;
+    }
+    
+    private void auto1() {
+        
+    }
+    
+    
+}
