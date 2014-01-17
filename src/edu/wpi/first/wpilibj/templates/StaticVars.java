2d7a7f32c608ee04574b37096b7ab79750f31d63
andrewdoyle2014 <andrewdoyle2014@lschs.org>
2014-01-17 14:33:26 -0500

    fixed jag channels

--- src/edu/wpi/first/wpilibj/templates/StaticVars.java
+++ src/edu/wpi/first/wpilibj/templates/StaticVars.java
@@ -5,8 +5,8 @@ public class StaticVars {
     
     //motor number channels
     public static int FRONT_LEFT_MOTOR = 1;
-    public static int FRONT_RIGHT_MOTOR = 2;
-    public static int REAR_LEFT_MOTOR = 3;
+    public static int REAR_LEFT_MOTOR = 2;
+    public static int FRONT_RIGHT_MOTOR = 3;
     public static int REAR_RIGHT_MOTOR = 4;
     
     //joystick  port number
