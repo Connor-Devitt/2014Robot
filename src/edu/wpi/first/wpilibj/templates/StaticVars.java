c8d5b0c2fa4f642ab47938679b4a1f01d82b2ef7
Robotics <Robotics@LSCHS-Robotics>
2014-01-24 15:17:28 -0500

    Fixed motor orientation; deleted switch comment

--- src/edu/wpi/first/wpilibj/templates/StaticVars.java
+++ src/edu/wpi/first/wpilibj/templates/StaticVars.java
@@ -4,10 +4,10 @@ package edu.wpi.first.wpilibj.templates;
 public class StaticVars {
     
     //motor number channels
-    public static int FRONT_LEFT_MOTOR = 1;
-    public static int REAR_LEFT_MOTOR = 2;
-    public static int FRONT_RIGHT_MOTOR = 3;
-    public static int REAR_RIGHT_MOTOR = 4;
+    public static int FRONT_LEFT_MOTOR = 2;
+    public static int REAR_LEFT_MOTOR = 4;
+    public static int FRONT_RIGHT_MOTOR = 1;
+    public static int REAR_RIGHT_MOTOR = 3;
     
     //joystick  port number
     public static int JOYSTICK_PORT = 1;
