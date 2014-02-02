c67a69899e524353495aa9a354615d1232cf4116
davidcox95 <davidcox95@192.168.6.183>
2014-02-02 00:58:44 -0500

    Created a DriveTrain class which overrides RobotDrive. This will hopefully be used to implement field-centric-navigation.

---
+++ src/edu/wpi/first/wpilibj/templates/DriveTrain.java
@@ -0,0 +1,25 @@
+
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Gyro;
+
+public class DriveTrain extends RobotDrive {
+
+    public DriveTrain(Actuators actuators) {
+        super(actuators.getMotorController(StaticVars.FRONT_LEFT_MOTOR),
+              actuators.getMotorController(StaticVars.REAR_LEFT_MOTOR),
+              actuators.getMotorController(StaticVars.FRONT_RIGHT_MOTOR),
+              actuators.getMotorController(StaticVars.REAR_RIGHT_MOTOR));
+    }
+    
+    void fieldDriveMecanumPolar(Gyro gyro, double magnitude, double direction, double rotation) {
+        //TODO: implement field-centric driving
+    }
+    
+    //DriveTrain testing method to be run during robotInitialization.
+    public void testDriveTrain() {
+        //TODO create a hardware test
+    }
+    
+}
