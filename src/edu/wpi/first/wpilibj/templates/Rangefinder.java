5a38174ef202a1460b145a08f2c15e6a14fae3a6
davidcox95 <davidcox95@192.168.6.183>
2014-02-11 17:06:55 -0500

    made code more complete

--- src/edu/wpi/first/wpilibj/templates/Rangefinder.java
+++ src/edu/wpi/first/wpilibj/templates/Rangefinder.java
@@ -8,11 +8,14 @@ import edu.wpi.first.wpilibj.Timer;
 //TODO implement!
 public class Rangefinder extends I2C {
     
-    DigitalModule digitalModule;
-    int sevenBitAddress;
+    private DigitalModule digitalModule;
+    private int sevenBitAddress;
+    private byte[] bytes = new byte[2];
     
     public Rangefinder(int addressHex) {
         super(DigitalModule.getInstance(StaticVars.RANGEFINDER_DIGITAL_MODULE), 0x70);
+        
+        Timer.delay(0.15);  //Boot delay of 150ms   make sure pin 1 is grounded (low).
         sevenBitAddress = 0x70;  //defaul address will change later in constructor..
         digitalModule = DigitalModule.getInstance(StaticVars.RANGEFINDER_DIGITAL_MODULE);
         
@@ -23,6 +26,10 @@ public class Rangefinder extends I2C {
         
         sevenBitAddress = addressHex/2; //0xH0 -> form of address to keep things simple
         Timer.delay(0.05);  //Time needed to reboot
+        
+    }
+    
+    private void readBytes() {
         
     }
     
