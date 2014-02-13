e93def5cc87877a489baf079f3fcddd29539c6d8
davidcox95 <davidcox95@192.168.6.183>
2014-02-12 20:25:32 -0500

    implemented new structure.

--- src/edu/wpi/first/wpilibj/templates/Team5181Robot.java
+++ src/edu/wpi/first/wpilibj/templates/Team5181Robot.java
@@ -22,43 +22,26 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 public class Team5181Robot extends IterativeRobot {
     
     //Global Declarations
-    //RobotHardware hardware;
-    
     //Autonomous autonomous;
-    //Actuators actuators;
-    //Sensors sensors;
-    //DriveTrain driveTrain;
+    Actuators actuators;
+    Sensors sensors;
+    DriveTrain driveTrain;
     //Turret turret;
     CustomJoystick joystick;
-    //RobotDrive used to drive robot
-    RobotDrive robotDrive;
-    
-    //Jaguar motor controllers used by robotDrive object
-    Talon frontLeft;
-    Talon rearLeft;
-    Talon frontRight;
-    Talon rearRight;
-    
     
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-        //hardware = new RobotHardware();
         //autonomous = new Autonomous(hardware);
         
-        //actuators = new Actuators();
-        //sensors = new Sensors();
-        //driveTrain = new DriveTrain(actuators);
+        actuators = new Actuators();
+        sensors = new Sensors();
+        driveTrain = new DriveTrain(actuators);
         joystick = new CustomJoystick();
-        frontLeft  = new Talon(StaticVars.FRONT_LEFT_MOTOR);
-        rearLeft   = new Talon(StaticVars.REAR_LEFT_MOTOR);
-        frontRight = new Talon(StaticVars.FRONT_RIGHT_MOTOR);
-        rearRight  = new Talon(StaticVars.REAR_RIGHT_MOTOR);
-        robotDrive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
-        //TestPeriodic initializations
         
+        /*
         LiveWindow.addActuator("Drive train", 
                              "front left motor", 
                              frontLeft);
@@ -71,12 +54,9 @@ public class Team5181Robot extends IterativeRobot {
         LiveWindow.addActuator("Drive train", 
                              "back right motor",
                              rearRight);
-        
+        */
     }
     
-    public void robotDisabled() {
-        
-    }
     /**
      * This function is called periodically during autonomous
      */
@@ -90,14 +70,11 @@ public class Team5181Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        LiveWindow.run();
         double direction = joystick.getDirectionDegrees();
         double magnitude = joystick.getMagnitude();
         double twist = joystick.getTwist();
         
-        //hardware.getRobotDrive().mecanumDrive_Polar(magnitude, direction, twist);
-        robotDrive.mecanumDrive_Polar(magnitude, direction, twist);
-        //driveTrain.fieldDriveMecanumPolar(sensors.getGryro(), magnitude, direction, twist);
+        driveTrain.fieldDriveMecanumPolar(sensors.getGryro(), magnitude, direction, twist);
         
     }
     
@@ -105,7 +82,7 @@ public class Team5181Robot extends IterativeRobot {
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-        //LiveWindow.run();
+        LiveWindow.run();
     }
     
 }
