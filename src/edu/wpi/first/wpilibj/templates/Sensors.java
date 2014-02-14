516b631b377b92dddb7947694d18f14e016cdfbf
davidcox95 <davidcox95@10.0.0.7>
2014-02-14 15:53:56 -0500

    added magLock spike relay, to be used on turret.

--- src/edu/wpi/first/wpilibj/templates/Sensors.java
+++ src/edu/wpi/first/wpilibj/templates/Sensors.java
@@ -3,19 +3,22 @@ package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Relay;
 
 public class Sensors {
 
     private final Encoder turretEncoder;
     private final Gyro gyro;
-    private final Rangefinder rangefinder;  //TODO implement Rangefinder class
+    private final Rangefinder rangefinder;
+    private final Relay magLockRelay;
     
     public Sensors() {
         turretEncoder = new Encoder(StaticVars.TURRET_ENCODER_A_CHANNEL,
                                     StaticVars.TURRET_ENCODER_B_CHANNEL);
         gyro = new Gyro(StaticVars.GYRO_CHANNEL);
         gyro.reset();   //zero the gyro
-        rangefinder = new Rangefinder(StaticVars.RANGEFINDER_8BIT_ADDRESS);
+        rangefinder = new Rangefinder();
+        magLockRelay = new Relay(StaticVars.MAG_LOCK_RELAY_CHANNEL);
     }
     
     public Gyro getGryro() {
@@ -24,6 +27,10 @@ public class Sensors {
     
     public Rangefinder getRangefinder() {
         return rangefinder;
+    }
+    
+    public Relay getMagLockRelay() {
+        return magLockRelay;
     }
     
 }
