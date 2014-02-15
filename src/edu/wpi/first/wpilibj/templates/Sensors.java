93945ca67f09b6f630ebd05fe8f713b665c9f4b8
davidcox95 <davidcox95@10.0.0.7>
2014-02-14 19:04:18 -0500

    added ball loader relay and ball loader limit switches.

--- src/edu/wpi/first/wpilibj/templates/Sensors.java
+++ src/edu/wpi/first/wpilibj/templates/Sensors.java
@@ -4,6 +4,7 @@ package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Sensors {
 
@@ -11,6 +12,9 @@ public class Sensors {
     private final Gyro gyro;
     private final Rangefinder rangefinder;
     private final Relay magLockRelay;
+    private final Relay ballLoadRelay;
+    private final DigitalInput ballLoadDownLimit;
+    private final DigitalInput ballLoadUpLimit;
     
     public Sensors() {
         turretEncoder = new Encoder(StaticVars.TURRET_ENCODER_A_CHANNEL,
@@ -19,6 +23,9 @@ public class Sensors {
         gyro.reset();   //zero the gyro
         rangefinder = new Rangefinder();
         magLockRelay = new Relay(StaticVars.MAG_LOCK_RELAY_CHANNEL);
+        ballLoadRelay = new Relay(StaticVars.BALL_LOAD_RELAY_CHENNEL);
+        ballLoadDownLimit = new DigitalInput(StaticVars.BALL_LOAD_DOWN_LIMIT_CHANNEL);
+        ballLoadUpLimit = new DigitalInput(StaticVars.BALL_LOAD_UP_LIMIT_CHANNEL);
     }
     
     public double getGyroAngle() {
@@ -40,6 +47,26 @@ public class Sensors {
     
     public void turnMagLockOn() {
         magLockRelay.set(Relay.Value.kOn);
+    }
+    
+    public void setBallLoadRelayForward() {
+        ballLoadRelay.set(Relay.Value.kForward);
+    }
+    
+    public void setBallLoadRelayReverse() {
+        ballLoadRelay.set(Relay.Value.kReverse);
+    }
+    
+    public void setballLoadRelayOff() {
+        ballLoadRelay.set(Relay.Value.kOff);
+    }
+    
+    public boolean ballLoadDownLimitReached() {
+        return ballLoadDownLimit.get();
+    }
+    
+    public boolean ballLoadUpLimitReached() {
+        return ballLoadDownLimit.get();
     }
     
 }
