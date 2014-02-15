037b9f20df96eb665ba7cfc935320edcb1414b85
davidcox95 <davidcox95@10.0.0.7>
2014-02-14 22:02:20 -0500

    implemented thread to get range in background.

--- src/edu/wpi/first/wpilibj/templates/Rangefinder.java
+++ src/edu/wpi/first/wpilibj/templates/Rangefinder.java
@@ -4,25 +4,49 @@ package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.DigitalModule;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SensorBase;
+import edu.wpi.first.wpilibj.Timer;
 
 //TODO implement!
 public class Rangefinder extends SensorBase {
     
     private I2C ranger;
+    private int latestDist;
+    private byte[] byteReturn;
+    private RangeThread rangeThread;
+        
     
     public Rangefinder() {
         ranger = DigitalModule.getInstance(1).getI2C(0xE0); //Use default shppd add
-        
+        latestDist = -1;
+        byteReturn = new byte[2];
+        rangeThread = new RangeThread();
     }
     
-    public void takeRange() {
-        ranger.write(0xE0, 0x51);   //take range reading add=0xE0, cmmd=0x51
+    private void takeRange() {
+        ranger.write(0xE0, 0x51);   //take range reading addr=0xE0, cmmd=0x51
     }
     
     //returns distance in centimeters...
-    public int getRange() {
-        byte[] byteReturn = new byte[2];
+    private int getRange() {
         ranger.read(0xE1, 2, byteReturn);
-        return (((int) byteReturn[0]) << 8) + ((int) byteReturn[1]);
+        return (int) ((byteReturn[0] << 8) | byteReturn[1]);
+    }
+    
+    public int getDistance() {
+        return latestDist;
+    }
+    
+    public void update() {
+        if (!rangeThread.isAlive())
+            rangeThread.start();
+    }
+    
+    private class RangeThread extends Thread {
+        public void run() {
+            takeRange();
+            Timer.delay(StaticVars.RANGE_DELAY);
+            latestDist = getRange();
+        }
+    
     }
 }
