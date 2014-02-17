52f8df9b03c1d29cf65152c6139aa54969253fe4
davidcox95 <davidcox95@192.168.6.183>
2014-02-17 14:17:44 -0500

    created handles for the turret.

--- src/edu/wpi/first/wpilibj/templates/Turret.java
+++ src/edu/wpi/first/wpilibj/templates/Turret.java
@@ -4,9 +4,21 @@ package edu.wpi.first.wpilibj.templates;
 public class Turret {
     
     String state;
+    Sensors sensors;
+    Actuators actuators;
     
-    public Turret() {
-        state = "stop";
+    public Turret(Sensors sensors, Actuators actuators) {
+        state = "wait";
+        this.sensors = sensors;
+        this.actuators = actuators;
+    }
+    
+    public void shoot() {
+        
+    }
+    
+    public void reload() {
+        
     }
     
 }
