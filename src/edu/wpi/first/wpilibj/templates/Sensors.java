e841b18dcaa0725b09c1dca75ebf008c6c85d4e9
Robotics <Robotics@192.168.3.10>
2014-02-22 15:04:00 -0500

    commented out gyro for debug

--- src/edu/wpi/first/wpilibj/templates/Sensors.java
+++ src/edu/wpi/first/wpilibj/templates/Sensors.java
@@ -1,38 +1,47 @@
 
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Sensors {
 
-    private final Gyro gyro;
+    //private final Gyro gyro;
     private final Rangefinder rangefinder;
-    private final DigitalInput ballLoadDownLimit;
-    private final DigitalInput ballLoadUpLimit;
+    //private final DigitalInput ballLoadDownLimit;
+    //private final DigitalInput ballLoadUpLimit;
+    //private final AnalogChannel gyroChannel;
     
     public Sensors() {
-        gyro = new Gyro(StaticVars.GYRO_CHANNEL);
+        /*
+        gyroChannel = new AnalogChannel(StaticVars.GYRO_CHANNEL);
+        gyro = new Gyro(gyroChannel);
+        gyro.setSensitivity(.007);
         gyro.reset();   //zero the gyro
-        
+        */
         
         rangefinder = new Rangefinder();
         rangefinder.checkRangefinder();
         rangefinder.startThread();
         
                 
-        ballLoadDownLimit = new DigitalInput(StaticVars.BALL_LOAD_DOWN_LIMIT_CHANNEL);
-        ballLoadUpLimit = new DigitalInput(StaticVars.BALL_LOAD_UP_LIMIT_CHANNEL);
+        //ballLoadDownLimit = new DigitalInput(StaticVars.BALL_LOAD_DOWN_LIMIT_CHANNEL);
+        //ballLoadUpLimit = new DigitalInput(StaticVars.BALL_LOAD_UP_LIMIT_CHANNEL);
     }
-    
+    /*
     public double getGyroAngle() {
         return gyro.getAngle();
+    }
+    
+    public double getGyroChannelVoltage() {
+        return gyroChannel.getVoltage();
     }
     
     public void resetGyro() {
         gyro.reset();
     }
-    
+    */
     //Returns int, measured in centimeters...
     
     public int getRangefinderDistance() {
@@ -42,7 +51,7 @@ public class Sensors {
    public void updateRangefinder() {
         rangefinder.update();
    }
-   
+    /*
     public boolean ballLoadDownLimitReached() {
         return ballLoadDownLimit.get();
     }
@@ -50,5 +59,5 @@ public class Sensors {
     public boolean ballLoadUpLimitReached() {
         return ballLoadUpLimit.get();
     }
-    
+    */
 }
