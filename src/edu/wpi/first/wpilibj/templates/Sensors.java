031be20fbd8d482771d1a8a15156f320e94f8f2d
davidcox95 <davidcox95@192.168.6.183>
2014-02-26 17:20:54 -0500

    uncommented and reinstated the gyro.

--- src/edu/wpi/first/wpilibj/templates/Sensors.java
+++ src/edu/wpi/first/wpilibj/templates/Sensors.java
@@ -1,42 +1,32 @@
 
 package edu.wpi.first.wpilibj.templates;
 
-import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.Gyro;
-import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Sensors {
 
-    //private final Gyro gyro;
+    private final Gyro gyro;
     private final Rangefinder rangefinder;
-    //private final AnalogChannel gyroChannel;
     
     public Sensors() {
-        /*
-        gyroChannel = new AnalogChannel(StaticVars.GYRO_CHANNEL);
-        gyro = new Gyro(gyroChannel);
+        
+        gyro = new Gyro(StaticVars.GYRO_CHANNEL);
         gyro.setSensitivity(.007);
         gyro.reset();   //zero the gyro
-        */
         
         rangefinder = new Rangefinder();
         rangefinder.checkRangefinder();
         rangefinder.startThread();
         
     }
-    /*
+    
     public double getGyroAngle() {
         return gyro.getAngle();
-    }
-    
-    public double getGyroChannelVoltage() {
-        return gyroChannel.getVoltage();
     }
     
     public void resetGyro() {
         gyro.reset();
     }
-    */
     
     /*
     * Returns int, measured in centimeters.
