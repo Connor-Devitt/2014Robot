97da450771f8725aebae2f108946654b33a89d18
davidcox95 <davidcox95@10.5.1.228>
2014-03-10 19:42:28 -0400

    added a ball retriever class to use actuators to intake the ball.

---
+++ src/edu/wpi/first/wpilibj/templates/BallRetriever.java
@@ -0,0 +1,27 @@
+
+package edu.wpi.first.wpilibj.templates;
+
+public class BallRetriever {
+    private final Actuators actuators;
+    
+    public BallRetriever(Actuators actuators) {
+        this.actuators = actuators;
+    }
+    
+    public void setBallRetrieverDown() {
+        
+    }
+    
+    public void setBallRetrieverUp() {
+        
+    }
+    
+    public void startIntakeWheels() {
+        
+    }
+    
+    public void stopIntakeWheels() {
+        
+    }
+    
+}
