7fb8b8ec7f5298405db5a404b3ebc6ac18e3b242
davidcox95 <davidcox95@192.168.6.183>
2014-03-12 22:28:06 -0400

    edited ball retriever class to use relays to move ball retriever.

--- src/edu/wpi/first/wpilibj/templates/BallRetriever.java
+++ src/edu/wpi/first/wpilibj/templates/BallRetriever.java
@@ -3,25 +3,44 @@ package edu.wpi.first.wpilibj.templates;
 
 public class BallRetriever {
     private final Actuators actuators;
+    private boolean intakeWheelsOn, intakeWheelsForward;
     
     public BallRetriever(Actuators actuators) {
         this.actuators = actuators;
+        intakeWheelsOn = false;
+        intakeWheelsForward = true;
     }
     
     public void setBallRetrieverDown() {
-        
+        actuators.setBallLoadPivotRelayForward();
     }
     
     public void setBallRetrieverUp() {
-        
+        actuators.setBallLoadPivotRelayReverse();
+    }
+    
+    public void setBallRetriverStop() {
+        actuators.setBallLoadPivotRelayOff();
     }
     
     public void startIntakeWheels() {
-        
+        actuators.setBallLoadWheelRelayForward();
     }
     
     public void stopIntakeWheels() {
-        
+        actuators.setBallLoadWheelRelayOff();
+    }
+    
+    public void startIntakeWheelsReverse() {
+        actuators.setBallLoadWheelRelayReverse();
+    }
+    
+    public boolean intakeWheelsForward() {
+        return intakeWheelsForward;
+    }
+    
+    public boolean intakeWheelsOn() {
+        return intakeWheelsOn;
     }
     
 }
